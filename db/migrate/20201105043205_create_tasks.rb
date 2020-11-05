fb60fc7d1c545a77f97fbcc294fa2923b03512dc
Chipo Jaya <chipojaya@gmail.com>
2020-11-05 06:02:30 +0000

    creating tables and columns for task model

--- db/migrate/20201105043205_create_tasks.rb
+++ db/migrate/20201105043205_create_tasks.rb
@@ -3,9 +3,6 @@ class CreateTasks < ActiveRecord::Migration[5.2]
     create_table :tasks do |t|
       t.string :title
       t.string :content
-      t.datetime :duedate
-      t.string :status
-      t.integer :priority
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -18,9 +18,6 @@ ActiveRecord::Schema.define(version: 2020_11_05_043205) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.string "content"
-    t.datetime "duedate"
-    t.string "status"
-    t.integer "priority"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
