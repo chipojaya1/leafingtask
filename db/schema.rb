30793679555a99dcb8e1ea4d378903a31670cb6c
Chipo Jaya <chipojaya@gmail.com>
2020-11-05 06:13:20 +0000

    adding columns to task

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_05_043205) do
+ActiveRecord::Schema.define(version: 2020_11_05_060513) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,9 @@ ActiveRecord::Schema.define(version: 2020_11_05_043205) do
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "duedate"
+    t.integer "priority"
+    t.string "status"
   end
 
 end
