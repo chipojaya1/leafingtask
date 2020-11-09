d2a10df3ab9eeb0ca9e548e0d6c4187a8c4adb85
Chipo Jaya <chipojaya@gmail.com>
2020-11-09 06:51:17 +0000

    japan localisation and model correction

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,4 @@
 class Task < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
-  validates :duedate, presence: true
-  validates :status, presence: true
-  validates :priority, presence: true
 end
