04f9a4510146f5661a2760b3de2d6e593d04eb2f
Chipo Jaya <chipojaya@gmail.com>
2020-11-13 12:26:14 +0000

    debugging features test

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,14 +1,10 @@
 FactoryBot.define do
-  factory :task_one do
+  factory :task do
     title { 'title test' }
     content { 'content test 1' }
   end
-  factory :task_two, class: Task do
+  factory :second_task, class: Task do
     title { 'title test 2' }
     content { 'content test 2' }
-  end
-  factory :task_three, class: Task do
-    title { 'title test 3' }
-    content { 'content test 3' }
   end
 end
--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
-
 RSpec.describe "Tasks management function", type: :system do
+
   describe 'new feautures' do
     context 'case was the task to create a new task' do
       it 'should display the new task created' do
@@ -12,22 +12,24 @@ RSpec.describe "Tasks management function", type: :system do
       end
     end
   end
+
   describe 'list function' do
     context 'to transition to the list screen' do
       it 'already created tasks should be displayed' do
-        test1 = FactoryBot.create(:task, title: 'test1')
+        task = FactoryBot.create(:task, title: 'task')
         visit tasks_path
-        expect(page).to have_content 'test1'
+        expect(page).to have_content 'task'
       end
     end
   end
+
   describe 'detailed display function' do
     context 'to transition to any task detail screen' do
-     it 'contents of relevant task should be displayed' do
-     test3 = FactoryBot.create(:task, title: 'test2', content: 'test2')
-       visit task_path ( test3 )
-       expect(page).to have_content 'test2'
-     end
+      it 'contents of relevant task should be displayed' do
+        task = FactoryBot.create(:task, title: 'test2', content: 'test2')
+        visit task_path(task)
+        expect(page).to have_content 'test2'
+      end
     end
   end
 end
