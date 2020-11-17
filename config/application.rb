5baa51a9950bcefd1402e74b50defd9049bbd39e
Chipo Jaya <chipojaya@gmail.com>
2020-11-17 10:00:16 +0000

    setting english and Japanese default languages and Tokyo timezone

--- config/application.rb
+++ config/application.rb
@@ -8,14 +8,9 @@ Bundler.require(*Rails.groups)
 
 module EverleafApp
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    config.load_defaults 5.2
     config.generators do |g|
       g.test_framework :rspec,
       model_specs: true,
