d69afc210471f9026d3d2a2e9a2b7aa480753eca
Chipo Jaya <chipojaya@gmail.com>
2020-11-18 08:10:15 +0000

    sorting by descending order and updating spec test

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -7,4 +7,8 @@ FactoryBot.define do
     title { 'title test 2' }
     content { 'content test 2' }
   end
+  factory :third_task, class: Task do
+    title { 'title test 3' }
+    content { 'content test 3' }
+  end
 end
--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe "Tasks management function", type: :system do
         fill_in "Task Name", with: 'title test'
         fill_in "Task Details", with: 'content test'
         click_on 'Create Task'
-        expect(page).to have_content 'content test'
+        expect(page).to have_content 'title test'
       end
     end
   end
@@ -17,7 +17,7 @@ RSpec.describe "Tasks management function", type: :system do
     context 'to transition to the list screen' do
       it 'already created tasks should be displayed' do
         task = FactoryBot.create(:task, title: 'task')
-        visit tasks_path
+        visit task_path(task)
         expect(page).to have_content 'task'
       end
     end
@@ -27,8 +27,29 @@ RSpec.describe "Tasks management function", type: :system do
     context 'to transition to any task detail screen' do
       it 'contents of relevant task should be displayed' do
         task = FactoryBot.create(:task, title: 'test2', content: 'test2')
-        visit task_path(task)
+        visit tasks_path
+        click_on 'Show'
         expect(page).to have_content 'test2'
+      end
+    end
+  end
+
+  let!(:task){ FactoryBot.create(:task, title: 'task') }
+  before do
+    FactoryBot.create(:task)
+    FactoryBot.create(:second_task)
+    FactoryBot.create(:third_task)
+    visit tasks_path
+  end
+  describe 'list display function' do
+    context 'When transitioning to the list screen' do
+      it 'already created tasks list should be displayed' do
+        expect(page).to have_content 'title test'
+      end
+    end
+    context 'When tasks are arranged in descending order of creation date and time' do
+      it 'New task is displayed at the top' do
+        expect(all('tbody tr')[0]).to have_content 'title test 3'
       end
     end
   end
