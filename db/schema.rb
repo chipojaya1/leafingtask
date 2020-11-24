17821995a15c080dac57018c17db4fde947b2500
Chipo Jaya <chipojaya@gmail.com>
2020-11-24 13:15:41 +0000

    adding a search index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_24_020811) do
+ActiveRecord::Schema.define(version: 2020_11_24_131155) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2020_11_24_020811) do
     t.datetime "duedate", null: false
     t.integer "priority", default: 0
     t.string "status", default: "pending"
+    t.index ["title"], name: "index_tasks_on_title"
   end
 
 end
