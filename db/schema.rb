f3e1ff3471940b292e920c2a5892ac0f256961a6
Chipo Jaya <chipojaya@gmail.com>
2020-12-01 19:05:26 +0000

    generatin controller midel and forms for user login function

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_01_183129) do
+ActiveRecord::Schema.define(version: 2020_12_01_190226) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2020_12_01_183129) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
