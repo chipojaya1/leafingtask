2f5196f1bbed4736fa7772960d8c623c40c07351
Chipo Jaya <chipojaya@gmail.com>
2020-12-08 01:50:46 +0000

    fixing logout and delete button

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  root 'tasks#index'
+  root 'users#new'
   resources :tasks
-  resources :users
-  resources :sessions
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users, only:[:new, :create, :show]
   namespace :admin do
-    resources :users
+    resources :users, only:[:index, :edit, :update, :new, :create, :show, :destroy]
   end
 end
